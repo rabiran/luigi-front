ca553f25d5aede87afdbac871e4685ce74f5f2a8
NoamYes <noamiko600@gmail.com>
2020-08-20 14:42:25 +0300

    Add fields input and submit

--- src/form.js
+++ src/form.js
@@ -1,34 +1,49 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 
 class MyForm extends React.Component {
+
     constructor(props) {
       super(props);
       this.state = {
         username: '',
         age: null,
       };
+      this.handleSubmit = this.onSubmit.bind(this);
     }
+
+    onSubmit = data => console.log(data);
+
     myChangeHandler = (event) => {
       let nam = event.target.name;
       let val = event.target.value;
       this.setState({[nam]: val});
     }
     render() {
+
       return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
         <h1>Hello {this.state.username} {this.state.age}</h1>
-        <p>Enter your name:</p>
+        <p>Enter Domain User:</p>
         <input
           type='text'
-          name='username'
+          name='DomainUser'
           onChange={this.myChangeHandler}
         />
-        <p>Enter your age:</p>
+        <p>Enter Identity Number:</p>
         <input
           type='text'
-          name='age'
+          name='Identity umber'
           onChange={this.myChangeHandler}
         />
+        <p>Enter Personal Number:</p>
+        <input
+          type='text'
+          name='PersonalNumber'
+          onChange={this.myChangeHandler}
+        />
+        <input type="submit" />
         </form>
       );
     }
